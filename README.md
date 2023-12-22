0392421c46e01d15e3089adcd3b6797c04c66132
nekosantk <24228731+nekosantk@users.noreply.github.com>
2023-12-22 17:10:12 +0200

    Added finishing touches + code cleanup

--- README.md
+++ README.md
@@ -1,1 +1,100 @@
 # python-metaprogramming-exercise
+
+The goals here is to implement a record structure that allows for an easy declarative programming structure. This is a toy exercise but utilizes techniques similar to projects like the Django ORM to allow for the behavior and structure of classes to be defined very declaratively. You must implement the Record class such that the fields are defined declaratively. The fields define a human readably label as well as a precondition which can check properties of a field on construction. The type of a field is defined by the attribute type annotation. An example is below:
+
+```python
+class Person(Record):
+    """
+    A simple person record
+    """ 
+    name: str = Field(label="The name") 
+    age: int = Field(label="The person's age", precondition=lambda x: 0 <= x <= 150)
+    income: float = Field(label="The person's income", precondition=lambda x: 0 <= x)
+```
+
+The requirements of the Record are:
+* It must generate a constructor that takes the appropriate keyword arguments.
+* If an argument is missing it must raise a TypeError
+* If an extra argument is given it must raise a TypeError
+* If the argument is given with the incorrect type it must raise a TypeError
+* The arguments must check the precondition is one is given, and raise a TypeError if the precondition is violated
+* It must expose the fields as readonly properties, and raise an AttributeError if an attempt is made to set them after construction.
+* It must customize the `__str__` method to create a pretty representation as shown below.
+* Inheritance should work in a sensible way.
+
+Example usage of the Person class would be:
+```python
+
+# This should work
+james = Person(name="James", age=34, income=24000.0)
+
+# This should raise a error
+james.age = 32
+```
+
+`print(str(james))` should output:
+
+```
+Person(
+  # The name
+  name='James'
+
+  # The person's age
+  age=34
+
+  # The person's income
+  income=24000.0
+)
+```
+
+With inheritance the following should be possible:
+
+```python
+class Named(Record):
+    """
+    A base class for things with names
+    """
+    name: str = Field(label="The name") 
+
+class Animal(Named):
+    """
+    An animal
+    """
+    habitat: str = Field(label="The habitat", precondition=lambda x: x in ["air", "land","water"])
+    weight: float = Field(label="The animals weight (kg)", precondition=lambda x: 0 <= x)
+
+class Dog(Animal):
+    """
+    A type of animal
+    """
+    bark: str = Field(label="Sound of bark")
+
+mike = Dog(name="mike", habitat="land", weight=50., bark="ARF")
+```
+
+There are probably a number of ways to skin this problem, but the solution must use metaclasses. Metaclasses allow for the programmer to intersect the class creation process. This is done through setting the metaclass attribute of a class, the outline would be:
+
+```python
+class RecordMeta(type):
+    def __new__(cls, name, bases, attr):
+        # Implement the class creation by manipulating the attr dictionary
+        return super(RecordMeta, cls).__new__(cls, name, bases, new_attr)
+
+# Set the metaclass of the Record class
+class Record(metaclass=RecordMeta):
+    pass
+```
+
+Inorder to get inheritance to work correctly you will need to use the Method Resolution Order (MRO) with the `cls.mro()` method. You can access the annotations with the `cls.__annotations__`. dictionary.
+
+A test suite is given in the tests.py file. This can be run with `python tests.py`. You should implement the Th Record and related classes in that file at the appropriate location.  All the test should pass.
+
+There are a bunch of concepts you will need to use:
+* Metaclasses
+* property()
+* cls.mro()
+* Type annotations
+* getattr/setattr
+* `__str__`
+
+It is not expected that you will know all these language features, but you should be able to figure them out. There is a wealth of information online about how these work. If anything is unclear please contact me. The implementation should not take more than 100 lines. 
--- tests.py
+++ tests.py
@@ -14,48 +14,74 @@ class Field:
     precondition: Callable[[Any], bool] = None
 
 
+# Record and supporting classes here
 
 class RecordMeta(type):
+    """
+    Metaclass for creating classes with aggregated fields and preconditions.
+    """
     def __new__(cls, name, bases, attrs):
-        fields = {}
+        aggregate_fields = {}
+
+        # Bottom up approach, prioritize fields in child
         for base in bases[::-1]:
             if hasattr(base, '_fields'):
-                fields.update(base._fields)
+                aggregate_fields.update(base._fields)
 
-        fields.update({key: value for key, value in attrs.items() if isinstance(value, Field)})
+        # Overwrite fields if necessary
+        aggregate_fields.update({key: value for key, value in attrs.items() if isinstance(value, Field)})
 
-        for field_name, field_value in fields.items():
+        # Add properties linked to field names during class creation with custom GET function
+        for field_name, field_value in aggregate_fields.items():
             attrs[field_name] = property(
                 fget=lambda self, name=field_name: self.get_value(name)
             )
 
-        attrs['_fields'] = fields
+        # Set fields
+        attrs['_fields'] = aggregate_fields
         return super().__new__(cls, name, bases, attrs)
 
 
-# Record and supporting classes here
 class Record(metaclass=RecordMeta):
     """
     Base class for records with fields and preconditions.
     """
     def __init__(self, **kwargs):
 
+        # Grab required fields + incoming keyword arguments
         required_keys = set(self._fields.keys())
         incoming_keys = set(kwargs.keys())
 
+        # Key checks, too few + too many
         if len(required_keys - incoming_keys) > 0: raise TypeError("Required keys are missing.")
         if len(incoming_keys - required_keys) > 0: raise TypeError("Too many keys were provided.")
 
+        # Grab required annotations
+        required_annotations = {}
+        for class_ in type(self).mro():
+            if hasattr(class_, "__annotations__"):
+                required_annotations.update(class_.__annotations__)
+
+        # Process incoming keyword arguments
         for field_name, field_value in kwargs.items():
+
+            # Plug incoming data into precondition checks
             precondition = getattr(self._fields.get(field_name), 'precondition', None)
             if precondition and not precondition(field_value):
                 raise TypeError(f"Precondition for field '{field_name}' is violated")
 
+            # Type check on fields
+            if not isinstance(field_value, required_annotations.get(field_name)):
+                raise TypeError(f"Field {field_name} type is incorrect.")
+
+            # Set real field data as private
             setattr(self, "_" + field_name, field_value)
 
+    # Custom get function for read only fields
     def get_value(self, name):
         return getattr(self, "_" + name)
 
+    # String representation
     def __str__(self):
         fields_str = "\n".join(
             [f"  # {self._fields[key].label}\n  {key}='{getattr(self, key)}'\n" if isinstance(getattr(self, key), str)
@@ -97,46 +123,44 @@ class Dog(Animal):
 
 # Tests
 class RecordTests(TestCase):
-    # def test_creation(self):
-    #     Person(name="JAMES", age=110, income=24000.0)
-    #     with self.assertRaises(TypeError):
-    #         Person(name="JAMES", age=160, income=24000.0)
-    #     with self.assertRaises(TypeError):
-    #         Person(name="JAMES")
-    #     with self.assertRaises(TypeError):
-    #         Person(name="JAMES", age=-1, income=24000.0)
-    #     with self.assertRaises(TypeError):
-    #         Person(name="JAMES", age="150", income=24000.0)
-    #     with self.assertRaises(TypeError):
-    #         Person(name="JAMES", age="150", wealth=24000.0)
+    def test_creation(self):
+        Person(name="JAMES", age=110, income=24000.0)
+        with self.assertRaises(TypeError):
+            Person(name="JAMES", age=160, income=24000.0)
+        with self.assertRaises(TypeError):
+            Person(name="JAMES")
+        with self.assertRaises(TypeError):
+            Person(name="JAMES", age=-1, income=24000.0)
+        with self.assertRaises(TypeError):
+            Person(name="JAMES", age="150", income=24000.0)
+        with self.assertRaises(TypeError):
+            Person(name="JAMES", age="150", wealth=24000.0)
 
     def test_properties(self):
         james = Person(name="JAMES", age=34, income=24000.0)
-        print("TEST", james.age)
-        print("TEST", james.name)
         self.assertEqual(james.age, 34)
         with self.assertRaises(AttributeError):
             james.age = 32
 
-    # def test_str(self):
-    #     james = Person(name="JAMES", age=34, income=24000.0)
-    #     correct = dedent("""
-    #     Person(
-    #       # The name
-    #       name='JAMES'
-    #
-    #       # The person's age
-    #       age=34
-    #
-    #       # The person's income
-    #       income=24000.0
-    #     )
-    #     """).strip()
-    #     self.assertEqual(str(james), correct)
+    def test_str(self):
+        james = Person(name="JAMES", age=34, income=24000.0)
+        correct = dedent("""
+        Person(
+          # The name
+          name='JAMES'
 
-    # def test_dog(self):
-    #     mike = Dog(name="mike", habitat="land", weight=50., bark="ARF")
-    #     self.assertEqual(mike.weight, 50)
+          # The person's age
+          age=34
+
+          # The person's income
+          income=24000.0
+        )
+        """).strip()
+        self.assertEqual(str(james), correct)
+
+    def test_dog(self):
+        mike = Dog(name="mike", habitat="land", weight=50., bark="ARF")
+        self.assertEqual(mike.weight, 50)
 
 
 if __name__ == '__main__':
